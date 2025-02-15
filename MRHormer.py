2781934af078eca45f7d21914965d62257ffbe5a
MENG Xiangrun <mc46720@um.edu.mo>
2025-02-15 20:22:06 +0800

    Update MRHormer.py

--- MRHormer.py
+++ MRHormer.py
@@ -1,1 +1,4 @@
-# wait
+# wait for 2025 ACM SIGKDD result
+# Association for Computing Machinery's Special Interest Group on Knowledge Discovery and Data Mining
+# Your Active Consoles
+# KDD 2025 Research Track February Authors
