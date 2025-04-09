4905d4f86e0ca329b55602686a0a002cd2ee277a
MENG Xiangrun <mc46720@um.edu.mo>
2025-04-09 20:11:05 +0800

    Add files via upload

--- MRHormer.py
+++ MRHormer.py
@@ -1,6 +1,9 @@
+import pandas as pd
 import torch_scatter
 import torch
 import torch_geometric as PyG
+import numpy as np
+from transformer import myTransformer
 
 
 class Linear(torch.nn.Module):
@@ -70,6 +73,11 @@ class TopologyRobustLocalAttention(torch.nn.Module):
 
         self.reset_parameters()
 
+        self.attention_dict = None
+        self.edge_index_dict = None
+
+        self.attention_type = 'sigmoid'  # softmax
+
     def reset_parameters(self):
         for edge_type in self.edge_type_list:
             torch.nn.init.kaiming_uniform_(self.head_weight[str(edge_type)])
@@ -94,9 +102,35 @@ class TopologyRobustLocalAttention(torch.nn.Module):
         offset_node_emb, node_range_dict, offset_edge_index, edge_range_dict = self.to_homogeneous(
             node_emb_dict=node_emb_dict, edge_index_dict=edge_index_dict)
 
+        # # message and aggregate
+        # attention_list = list()
+        # message_list = list()
+        # for edge_type, edge_index in edge_index_dict.items():
+        #     source_type = edge_type[0]
+        #     target_type = edge_type[-1]
+        #
+        #     source_index = edge_index[0]
+        #     target_index = edge_index[1]
+        #
+        #     source_emb = node_emb_dict[source_type]
+        #     target_emb = node_emb_dict[target_type]
+        #
+        #     k_emb = self.k_linear[str(edge_type)](source_emb)[source_index]
+        #     q_emb = self.q_linear[str(edge_type)](target_emb)[target_index]
+        #
+        #     attention = torch.cat(tensors=[k_emb, q_emb], dim=1)
+        #     attention = self.a_linear[str(edge_type)](attention)
+        #     attention = attention.view(-1, self.num_head, self.head_dim)
+        #     attention = attention * self.head_weight[str(edge_type)]
+        #     attention_list.append(attention)
+        #
+        #     v_emb = self.v_linear[str(edge_type)](source_emb)[source_index]
+        #     v_emb = v_emb.view(-1, self.num_head, self.head_dim)
+        #     message_list.append(v_emb)
+
         # message and aggregate
-        attention_list = list()
-        message_list = list()
+        attention_dict = dict()
+        message_dict = dict()
         for edge_type, edge_index in edge_index_dict.items():
             source_type = edge_type[0]
             target_type = edge_type[-1]
@@ -114,33 +148,27 @@ class TopologyRobustLocalAttention(torch.nn.Module):
             attention = self.a_linear[str(edge_type)](attention)
             attention = attention.view(-1, self.num_head, self.head_dim)
             attention = attention * self.head_weight[str(edge_type)]
-
-            # sigmoid
-            attention = torch.nn.functional.sigmoid(attention)
-
-            attention_list.append(attention)
+            attention_dict[edge_type] = attention
 
             v_emb = self.v_linear[str(edge_type)](source_emb)[source_index]
             v_emb = v_emb.view(-1, self.num_head, self.head_dim)
-            message_list.append(v_emb)
+            message_dict[edge_type] = v_emb
 
+        # aggregate
+        attention = torch.cat(list(attention_dict.values()), dim=0)
+        message = torch.cat(list(message_dict.values()), dim=0)
+
+        # aggregate
         num_nodes = offset_node_emb.shape[0]
         source_index = offset_edge_index[0]
         target_index = offset_edge_index[1]
 
-        # aggregate
-        attention = torch.cat(attention_list, dim=0)
-        del attention_list
-
-        message = torch.cat(message_list, dim=0)
-        del message_list
-
-        # softmax
-        # attention = PyG.utils.softmax(src=attention, index=target_index, num_nodes=num_nodes, dim=0)
+        if self.attention_type == 'sigmoid':
+            attention = torch.nn.functional.sigmoid(attention)
+        if self.attention_type == 'softmax':
+            attention = PyG.utils.softmax(src=attention, index=target_index, num_nodes=num_nodes, dim=0)
 
         message = attention * message
-
-        # del attention
         message = message.view(-1, self.hidden_dim)
         node_emb = torch_scatter.scatter_add(src=message, index=target_index, dim=0, dim_size=num_nodes)
         del message
@@ -211,9 +239,100 @@ class TopologyRobustLocalAttention(torch.nn.Module):
 
         return node_emb, node_range_dict, offset_edge_index, edge_range_dict
 
+    def visualization(self, x_dict, edge_index_dict, index2node_dict, dataset_name, data_type):
+        node_emb_dict = x_dict.copy()
+
+        if self.is_selfloop:
+            for node_type, node_emb in node_emb_dict.items():
+                num_node = node_emb.shape[0]
+                edge_index = torch.arange(0, num_node, device=node_emb.device).view(1, -1).repeat(2, 1)
+                edge_type = (node_type, 'SelfLoop', node_type)
+                edge_index_dict[edge_type] = edge_index
+
+            for edge_type in self.edge_type_list:
+                assert edge_type in edge_index_dict.keys()
+
+        for node_type, node_emb in node_emb_dict.items():
+            node_emb_dict[node_type] = self.in_linear[node_type](node_emb)
+
+        # reset edge index to homogeneous
+        offset_node_emb, node_range_dict, offset_edge_index, edge_range_dict = self.to_homogeneous(
+            node_emb_dict=node_emb_dict, edge_index_dict=edge_index_dict)
+
+        attention_dict = dict()
+        for edge_type, edge_index in edge_index_dict.items():
+            source_type = edge_type[0]
+            target_type = edge_type[-1]
+
+            source_index = edge_index[0]
+            target_index = edge_index[1]
+
+            source_emb = node_emb_dict[source_type]
+            target_emb = node_emb_dict[target_type]
+
+            k_emb = self.k_linear[str(edge_type)](source_emb)[source_index]
+            q_emb = self.q_linear[str(edge_type)](target_emb)[target_index]
+
+            attention = torch.cat(tensors=[k_emb, q_emb], dim=1)
+            attention = self.a_linear[str(edge_type)](attention)
+            attention = attention.view(-1, self.num_head, self.head_dim)
+            attention = attention * self.head_weight[str(edge_type)]
+            attention_dict[edge_type] = attention
+
+        # aggregate
+        attention = torch.cat(list(attention_dict.values()), dim=0)
+
+        # aggregate
+        num_nodes = offset_node_emb.shape[0]
+        source_index = offset_edge_index[0]
+        target_index = offset_edge_index[1]
+
+        if self.attention_type == 'sigmoid':
+            attention = torch.nn.functional.sigmoid(attention)
+        if self.attention_type == 'softmax':
+            attention = PyG.utils.softmax(src=attention, index=target_index, num_nodes=num_nodes, dim=0)
+
+        for edge_type, edge_range in edge_range_dict.items():
+            start, end = edge_range
+            attention_dict[edge_type] = attention[start, end + 1]
+
+        for edge_type in edge_index_dict.items():
+            source_type, target_type = edge_type[0], edge_type[-1]
+            num_source = node_emb_dict[source_type].shape[0]
+            num_target = node_emb_dict[target_type].shape[0]
+            num_edge = edge_index_dict[edge_type].shape[1]
+
+            attention = attention_dict[edge_type]
+            attention = attention.reshape(num_edge, -1)
+            attention = torch.mean(attention, dim=1)
+
+            edge_index = edge_index_dict[edge_type].T.detach().cpu().numpy()
+            attention = attention.detach().cpu().numpy()
+
+            edge_index = pd.DataFrame(edge_index)
+            attention = pd.DataFrame(attention)
+
+            edge_attention = pd.concat([edge_index, attention], columns=[source_type, target_type, 'attention'])
+
+            adj_matrix = np.zeros((num_source, num_target), dtype=int)
+
+            for index, row in edge_attention.iterrows():
+                source = row[source_type]
+                target = row[target_type]
+                weight = row['attention']
+                adj_matrix[source, target] = weight
+
+            source_node_name = list(index2node_dict[source_type].values())
+            target_node_name = list(index2node_dict[target_type].values())
+
+            adj_matrix = pd.DataFrame(adj_matrix, index=source_node_name, columns=target_node_name)
+
+            filename = f'{dataset_name}_{data_type}_{source_type}_{target_node_name}_{self.attention_type}_local_att.xlsx'
+            adj_matrix.to_excel(filename, index=True)
+
 
 class MultiRelationGlobalAttention(torch.nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim, node_type_list, edge_type_list, num_head, self_layers):
+    def __init__(self, config, in_dim, hidden_dim, out_dim, node_type_list, edge_type_list, num_head, self_layers):
         super().__init__()
         self.in_dim = in_dim
         self.hidden_dim = hidden_dim
@@ -230,12 +349,11 @@ class MultiRelationGlobalAttention(torch.nn.Module):
         for node_type in node_type_list:
             self.in_linear[node_type] = Linear(self.hidden_dim)
             self.out_linear[node_type] = Linear(self.out_dim)
-            self.global_attention[node_type] = torch.nn.Transformer(d_model=self.hidden_dim,
-                                                                    nhead=self.num_head,
-                                                                    num_encoder_layers=self_layers,
-                                                                    num_decoder_layers=self_layers,
-                                                                    batch_first=False)
-
+            self.global_attention[node_type] = myTransformer(config=config)
+            # self.global_attention[node_type] = torch.nn.Transformer(d_model=self.hidden_dim,
+            #                                                         num_encoder_layers=self_layers,
+            #                                                         num_decoder_layers=self_layers,
+            #                                                         batch_first=True,)
 
         self.target_source_dict = {}
         for edge_type in edge_type_list:
@@ -261,10 +379,11 @@ class MultiRelationGlobalAttention(torch.nn.Module):
             source_emb = torch.cat(source_emb_list, dim=0)
             target_emb = h_dict[target_type]
 
-            target_emb = self.global_attention[target_type](src=source_emb, tgt=target_emb)
+            target_emb, attention = self.global_attention[target_type](source_emb, target_emb)
+            # target_emb = self.global_attention[target_type](source_emb, target_emb)
             # target_emb = self.global_attention[target_type](key=source_emb, query=target_emb, value=source_emb)[0]
 
-            node_emb_dict[target_type] =target_emb
+            node_emb_dict[target_type] = target_emb
 
         for node_type, node_emb in node_emb_dict.items():
             node_emb_dict[node_type] = self.out_linear[node_type](node_emb)
@@ -277,13 +396,13 @@ class MRHormer(torch.nn.Module):
                  config):
         super().__init__()
         self.config = config
-        self.node_type_list=config.node_type_list
-        self.edge_type_list=config.edge_type_list
+        self.node_type_list = config.node_type_list
+        self.edge_type_list = config.edge_type_list
         self.is_selfloop = config.is_selfloop
 
-        self.hidden_dim=config.encoder_hidden_dim
-        self.num_local_head=config.num_local_head
-        self.num_global_head=config.num_global_head
+        self.hidden_dim = config.encoder_hidden_dim
+        self.num_local_head = config.num_local_head
+        self.num_global_head = config.num_global_head
 
         self.num_local_layer = config.num_local_layer
         self.num_global_self_layer = config.num_global_self_layer
@@ -319,11 +438,10 @@ class MRHormer(torch.nn.Module):
                                                      num_head=self.num_global_head,
                                                      self_layers=self.num_global_self_layer,
                                                      node_type_list=self.node_type_list,
-                                                     edge_type_list=self.edge_type_list)
+                                                     edge_type_list=self.edge_type_list,
+                                                     config=config)
         else:
             self.MRGA = None
-
-
 
     def forward(self, x_dict, edge_index_dict):
         # local
@@ -336,7 +454,6 @@ class MRHormer(torch.nn.Module):
         if self.MRGA is not None:
             h_global_dict = x_dict.copy()
             h_global_dict = self.MRGA.forward(x_dict=h_global_dict)
-
 
         if len(self.TRLA) > 0 and self.MRGA is None:
             h_dict = h_local_dict
@@ -352,6 +469,3 @@ class MRHormer(torch.nn.Module):
                 h_dict[node_type] = torch.cat([h_local, h_global], dim=1)
 
         return h_dict
-
-
-
---
+++ transformer.py
@@ -0,0 +1,182 @@
+import torch_geometric
+import torch
+import math
+
+
+class Linear(torch.nn.Module):
+    def __init__(self, out_dim, bias=True):
+        super().__init__()
+        self.out_dim = out_dim
+        self.linear = torch_geometric.nn.Linear(in_channels=-1,
+                                                out_channels=self.out_dim,
+                                                weight_initializer='kaiming_uniform',
+                                                bias=bias,
+                                                bias_initializer='zeros')
+        self.linear.reset_parameters()
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+class MHA(torch.nn.Module):
+    def __init__(self,
+                 emb_dim,
+                 num_head,
+                 dropout=0.0):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.num_head = num_head
+        self.dropout = dropout
+        self.head_dim = self.emb_dim // self.num_head
+        assert self.head_dim * self.num_head == self.emb_dim
+
+        self.q_linear = Linear(self.emb_dim, bias=False)
+        self.k_linear = Linear(self.emb_dim, bias=False)
+        self.v_linear = Linear(self.emb_dim, bias=False)
+        self.out_linear = Linear(self.emb_dim, bias=True)
+
+    def forward(self,
+                source_emb,
+                target_emb,
+                attention_mask=None):
+        if source_emb.ndim == 2:
+            num_source_token, source_emb_dim = source_emb.shape
+            source_emb = source_emb.view(1, num_source_token, source_emb_dim)
+        if target_emb.ndim == 2:
+            num_target_token, target_emb_dim = target_emb.shape
+            target_emb = target_emb.view(1, num_target_token, target_emb_dim)
+
+        batch_size, num_source_token, source_emb_dim = source_emb.shape
+        batch_size, num_target_token, target_emb_dim = target_emb.shape
+
+        num_head = self.num_head
+        head_dim = self.head_dim
+
+        q_emb = target_emb.transpose(1, 0)
+        q_emb = self.q_linear(q_emb)
+        q_emb = q_emb.view(num_target_token, batch_size, num_head, head_dim)
+        q_emb = q_emb.view(num_target_token, batch_size * num_head, head_dim)
+        q_emb = q_emb.transpose(0, 1)
+
+        k_emb = source_emb.transpose(1, 0)
+        num_source_token, batch_size, source_emb_dim = k_emb.shape
+        k_emb = self.k_linear(k_emb)
+        k_emb = k_emb.view(num_source_token, batch_size, num_head, head_dim)
+        k_emb = k_emb.view(num_source_token, batch_size * num_head, head_dim)
+        k_emb = k_emb.transpose(0, 1)
+        k_emb_transpose = k_emb.transpose(-2, -1)
+
+        v_emb = source_emb.transpose(1, 0)
+        num_source_token, batch_size, source_emb_dim = v_emb.shape
+        v_emb = self.v_linear(v_emb)
+        v_emb = v_emb.view(num_source_token, batch_size, num_head, head_dim)
+        v_emb = v_emb.view(num_source_token, batch_size * num_head, head_dim)
+        v_emb = v_emb.transpose(0, 1)
+
+        # attention_mask
+        # (batch_size * num_head, num_target_token, num_source_token)
+        if attention_mask is not None:
+            q_emb = q_emb / math.sqrt(float(head_dim))  # scaling
+            attention = torch.baddbmm(attention_mask, q_emb, k_emb_transpose)
+        else:
+            q_emb = q_emb / math.sqrt(float(head_dim))  # scaling
+            attention = torch.bmm(q_emb, k_emb.transpose(-2, -1))
+
+        attention = torch.nn.functional.softmax(attention, dim=-1)
+        attention = torch.nn.functional.dropout(input=attention, p=self.dropout, training=self.training)
+
+        out_emb = torch.bmm(attention, v_emb)
+        out_emb = out_emb.transpose(dim0=0, dim1=1).contiguous()
+        out_emb = out_emb.view(num_target_token, batch_size, num_head, head_dim)
+        out_emb = out_emb.view(num_target_token, batch_size, target_emb_dim)
+        out_emb = out_emb.transpose(dim0=0, dim1=1).contiguous()
+        out_emb = self.out_linear(out_emb)
+
+        attention = attention.view(batch_size, num_head, num_target_token, num_source_token)
+
+        if batch_size == 1:
+            mean_attention = attention.mean(dim=1)
+            attention = mean_attention.squeeze(0)
+            out_emb = out_emb.squeeze(0)
+        return out_emb, attention
+
+
+class FFN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim=2048, dropout_p=0.1):
+        super().__init__()
+        self.linear1 = Linear(hidden_dim)
+        self.linear2 = Linear(input_dim)  # Output dimension should match input
+        self.activation = torch.nn.ReLU()  # Or any other activation function you prefer
+        self.dropout = torch.nn.Dropout(dropout_p)
+        self.dropout3 = torch.nn.Dropout(dropout_p)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        return self.dropout3(x)
+
+
+
+class myTransformer(torch.nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.num_layer = config.num_global_self_layer
+
+        self.source_layers = torch.nn.ModuleList()
+        for _ in range(self.num_layer ):
+            source_mha = MHA(emb_dim=config.encoder_hidden_dim, num_head=config.num_global_head)
+            self.source_layers.append(source_mha)
+        self.source_ffn = FFN(input_dim=config.encoder_hidden_dim)
+        self.source_norm1 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+        self.source_norm2 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+
+        self.target_layers = torch.nn.ModuleList()
+        for _ in range(self.num_layer):
+            target_mha = MHA(emb_dim=config.encoder_hidden_dim, num_head=config.num_global_head)
+            self.target_layers.append(target_mha)
+        self.target_ffn = FFN(input_dim=config.encoder_hidden_dim)
+        self.target_norm1 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+        self.target_norm2 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+
+        self.cross_layers = torch.nn.ModuleList()
+        for _ in range(self.num_layer ):
+            cross = MHA(emb_dim=config.encoder_hidden_dim, num_head=config.num_global_head)
+            self.cross_layers.append(cross)
+        self.cross_ffn = FFN(input_dim=config.encoder_hidden_dim)
+        self.cross_norm1 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+        self.cross_norm2 = torch.nn.LayerNorm(config.encoder_hidden_dim, eps=1e-5, bias=True)
+
+    def forward(self, source_emb, target_emb):
+        for layer_index in range(self.num_layer):
+            residual = source_emb.clone()
+            source_emb, _ = self.source_layers[layer_index].forward(source_emb=source_emb, target_emb=source_emb)
+            source_emb += residual
+            source_emb = self.source_norm1(source_emb)
+
+            residual = source_emb.clone()
+            source_emb = self.source_ffn(source_emb)
+            source_emb += residual
+            source_emb = self.source_norm2(source_emb)
+
+        for layer_index in range(self.num_layer):
+            # residual = target_emb.clone()
+            # target_emb, _ = self.target_layers[layer_index].forward(source_emb=target_emb, target_emb=target_emb)
+            # target_emb += residual
+            # target_emb = self.target_norm1(target_emb)
+            #
+            # residual = target_emb.clone()
+            # target_emb = self.target_ffn(target_emb)
+            # target_emb += residual
+            # target_emb = self.target_norm2(target_emb)
+            #
+            # for layer_index in range(self.num_layer):
+            residual = target_emb.clone()
+            target_emb, attention = self.cross_layers[layer_index].forward(source_emb=source_emb, target_emb=target_emb)
+            target_emb += residual
+            target_emb = self.cross_norm1(target_emb)
+
+            residual = target_emb.clone()
+            target_emb = self.cross_ffn(target_emb)
+            target_emb += residual
+            target_emb = self.cross_norm2(target_emb)
+
+        return target_emb, attention
